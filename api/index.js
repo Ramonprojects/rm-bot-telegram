32602c39fa2abf934b0a317f3122f5a18b5d87fa
Ramonprojects <adm@ramonmago.com>
2026-06-24 22:39:29 -0300

    chore: shareCode DXZ76MO53KC -> C4I4R6W3BND (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=DXZ76MO53KC'
+    const shareCode = '&shareCode=C4I4R6W3BND'
     
     const afp = '&afp4=bot'
     
