32bca4ff8b575b760c97b22564aefdc8b7b73f69
Ramonprojects <adm@ramonmago.com>
2026-06-28 22:02:50 -0300

    chore: shareCode NKW9DAB072J -> SXFF6S1UJJN (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=NKW9DAB072J'
+    const shareCode = '&shareCode=SXFF6S1UJJN'
     
     const afp = '&afp4=bot'
     
