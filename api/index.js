209cd42c33509588d1bef5995014cb1c2e943a8a
Ramonprojects <adm@ramonmago.com>
2026-07-11 23:14:40 -0300

    chore: shareCode R8PYS5U764W -> ATDR1LFE6FQ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=R8PYS5U764W'
+    const shareCode = '&shareCode=ATDR1LFE6FQ'
     
     const afp = '&afp4=bot'
     
