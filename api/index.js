e24a81eb0fd195d01d22f3ca0bfbbfe72d6eba1a
Ramonprojects <adm@ramonmago.com>
2026-07-13 10:15:20 -0300

    chore: shareCode ATDR1LFE6FQ -> HCLX4FVAUSR (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=ATDR1LFE6FQ'
+    const shareCode = '&shareCode=HCLX4FVAUSR'
     
     const afp = '&afp4=bot'
     
