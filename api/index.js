b850992215685fb5f34f8f6dfa69b1d7a6dc76f3
Ramonprojects <adm@ramonmago.com>
2026-07-14 12:52:31 -0300

    chore: shareCode R0RYPBCTPUU -> K9J8GER4ZDD (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=R0RYPBCTPUU'
+    const shareCode = '&shareCode=K9J8GER4ZDD'
     
     const afp = '&afp4=bot'
     
