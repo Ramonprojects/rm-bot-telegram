088a8f16c6a767be3ff4636df4916a0a892e49d6
Ramonprojects <adm@ramonmago.com>
2026-08-26 03:44:23 -0300

    chore: shareCode 7EC39L3K3MR -> WCFBPINAFZN (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=7EC39L3K3MR'
+    const shareCode = '&shareCode=WCFBPINAFZN'
     
     const afp = '&afp4=bot'
     
