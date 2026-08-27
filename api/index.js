018ae9a9c1e024d3a76fecf32c4424c05a84b92b
Ramonprojects <adm@ramonmago.com>
2026-08-27 02:24:45 -0300

    chore: shareCode S5DNSAEM5XZ -> P5JEXR1IPFO (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=S5DNSAEM5XZ'
+    const shareCode = '&shareCode=P5JEXR1IPFO'
     
     const afp = '&afp4=bot'
     
