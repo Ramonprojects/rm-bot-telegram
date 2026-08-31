7f92c26892ffbdfa90acb420e2bf63e2b17c9794
Ramonprojects <adm@ramonmago.com>
2026-08-31 09:09:18 -0300

    chore: shareCode NZ8MJJKEHAK -> X0IP8Y99SKE (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=NZ8MJJKEHAK'
+    const shareCode = '&shareCode=X0IP8Y99SKE'
     
     const afp = '&afp4=bot'
     
