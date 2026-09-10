d570a15d147d98166b28cabb0315a357d4437066
Ramonprojects <adm@ramonmago.com>
2026-09-10 01:25:24 -0300

    chore: shareCode 2HPKRCA15MD -> LIHJKA2J9IZ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29908'
 
-    const shareCode = '&shareCode=2HPKRCA15MD'
+    const shareCode = '&shareCode=LIHJKA2J9IZ'
     
     const afp = '&afp4=bot'
     
